9482b12fcdc770606a32ded81fb532dd2a1eb3d8
Sam Clements <sam@borntyping.co.uk>
2023-11-29 15:13:41 +0000

    Write logs to STDERR to avoid conflicting with export commands

--- vancelle/ext/structlog.py
+++ vancelle/ext/structlog.py
@@ -1,3 +1,5 @@
+import sys
+
 import structlog
 
 
@@ -8,4 +10,5 @@ def configure_logging():
             structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
             structlog.dev.ConsoleRenderer(pad_event=0, sort_keys=False),
         ],
+        logger_factory=structlog.PrintLoggerFactory(sys.stderr),
     )

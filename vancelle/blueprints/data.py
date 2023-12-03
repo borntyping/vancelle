cef2d30ad249bae088d7b7500850ec04e8693f4c
Sam Clements <sam@borntyping.co.uk>
2023-12-03 01:11:20 +0000

    Remove notion blueprint

--- vancelle/blueprints/data.py
+++ vancelle/blueprints/data.py
@@ -1,14 +1,12 @@
 import flask
 import flask_login
 
-from .goodreads import bp as bp_goodreads
-from .notion import bp as bp_notion
+from vancelle.blueprints.goodreads import bp as bp_goodreads
 
 bp = flask.Blueprint("data", __name__)
 bp.cli.short_help = "Import and export data."
 
 bp.register_blueprint(bp_goodreads)
-bp.register_blueprint(bp_notion)
 
 
 @bp.before_request
